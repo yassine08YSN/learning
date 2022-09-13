676815b1b191d7e635164a4c908e9401950de635
yassine08YSN <chatouiyassine2022@gmail.com>
2022-09-13 22:40:59 +0200

    doing exercices

---
+++ task.py
@@ -0,0 +1,85 @@
+#● Create a Boolean variable named x
+from re import T
+
+
+x = True
+print(type(x))
+
+#Create an integer variable named y
+y = 56
+print(type(y))
+
+#Create a float variable named z
+z = 43.75
+print(type(z))
+
+#Create a string variable names s
+s = "Yassine"
+print(type(s))
+
+#Convert the int variable to float
+t = 2008
+print(float(t))
+
+#Can we convert the str to int ?
+#yeeeess!!!
+#the proof:
+j = "45645"
+print(int(j))
+
+#Create a list of numbers from 1 to 5
+l = [1 , 2 , 3 , 4 , 5]
+print(type(l))
+
+#Create a tuple from 10 to 15
+m = 10, 11 , 12 , 13 , 14 ; 15
+print(type(m))
+
+#Convert the list to tuple
+k = [1 , 2 , 3 , 4 , 5 , 6]
+print(tuple(k))
+
+#Create a dict of 3 values
+o = {1:"yassine" , 8:"chtoui", 9:"7amid"}
+print(type(o))
+
+#Can we use semi colon ; with python
+
+#Nooo , some others language use semi colons , like :
+#  c++ , java ...
+# but python donsn't use it .
+
+'''
+● Python is interpreted or compiled ?
+● What is the differences between low level & high level
+● What is the differences between = , ==
+● What do we mean by using !=
+● What is the operator precedence
+● Create a variable x with value of 10
+● Increase x value by 15 using assignment operators
+● Divide the x value by 5 using assignment operators
+● Multiply the x value by 10 using assignment operator
+● Get module of x on 3 using assignment operators
+● Using python print the module of 11 to 4
+● Print the exponent of 2,3
+● Divide 11 by 3 using python division
+● Can we divide 11 by 3 and get an integer number ?
+● Check if 10 is bigger than 15 or not
+● If 10 is not bigger than15 print x is smaller than 15
+● In which cases we will use all
+● What is the differences between all , and
+● What is the differences between any , or
+● If we need all the conditions to be true we will use ….
+● What is the differences between if , elif
+● What is the differences between elif else
+● Can we use more than one elif
+● Can we use more than one else
+● write s simple ternary operator
+● in elif , python will check all the conditions no matter what ?
+● in elif we use else for ... ?
+● if we have this list [2,4,6,8,10] :
+○ check to see if 4 in this list or not
+○ check to see if 4 and 6 in this list on not
+○ check to see if 3 or 6 in this list
+○ check to see if 2 , 4 and 5 in this list
+'''
